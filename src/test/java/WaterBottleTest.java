b94b3991043c6c9f054e5abe4b053218c9bd3ffd
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2021-01-25 17:47:02 +0000

    wrote and passed waterBottle test, volShouldStartAt100

--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+
+    WaterBottle watbot;
+
+    @Before
+    public void before(){
+        watbot = new WaterBottle();
+    }
+
+    @Test
+    public void volShouldStartAt100(){
+        assertEquals(100, watbot.getVolume());
+    }
 }
