407a599f5838bc6013f7d1394b7fd68520469c6a
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2021-01-25 17:55:07 +0000

    wrote and passed final water bottle tests

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -9,4 +9,16 @@ public class WaterBottle {
     public int getVolume(){
         return volume;
     }
+
+    public void drink(){
+        volume -= 10;
+    }
+
+    public void empty(){
+        volume = 0;
+    }
+
+    public void fill(){
+        volume = 100;
+    }
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -16,4 +16,28 @@ public class WaterBottleTest {
     public void volShouldStartAt100(){
         assertEquals(100, watbot.getVolume());
     }
+
+    @Test
+    public void drinkShouldDecreaseVol(){
+        watbot.drink();
+        assertEquals(90, watbot.getVolume());
+        watbot.drink();
+        assertEquals(80, watbot.getVolume());
+    }
+
+    @Test
+    public void emptyShouldEmptyVol(){
+        assertEquals(100, watbot.getVolume());
+        watbot.empty();
+        assertEquals(0, watbot.getVolume());
+    }
+
+    @Test
+    public void fillShouldFillVol(){
+        assertEquals(100, watbot.getVolume());
+        watbot.empty();
+        assertEquals(0, watbot.getVolume());
+        watbot.fill();
+        assertEquals(100, watbot.getVolume());
+    }
 }
