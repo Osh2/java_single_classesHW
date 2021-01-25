f0f1c40b7ba7a80f2355df1ba03d54d2231bd2cf
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2021-01-25 17:29:39 +0000

    wrote and passed canAdd() test

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd(){
+        assertEquals(10, calculator.add(8,2));
+    }
+
+    @Test
+    public void canSubtract(){
+        assertEquals(6, calculator.subtract(8, 2));
+    }
+}
