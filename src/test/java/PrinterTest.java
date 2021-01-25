ca6a6afb937c7c48224209d677ba09a104d2c28e
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2021-01-25 17:59:25 +0000

    wrote and passed shouldHavePaperLeftProp test for printer class

--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
+
+    Printer printer;
+
+    @Before
+    public void before(){
+        printer = new Printer(82);
+    }
+
+    @Test
+    public void shouldHavePaperLeftProp(){
+        assertEquals(82, printer.getSheets());
+    }
 }
